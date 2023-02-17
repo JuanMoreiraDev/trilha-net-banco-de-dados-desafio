11304907448e52ff2304aa91bd4812916732b259
JuanMoreiraDev <roque.li@outlook.com>
2023-02-17 14:51:00 -0300

    Feito

---
+++ resolução.sql
@@ -0,0 +1,10 @@
+SELECT Nome,Ano FROM Filmes
+
+SELECT Nome,Ano,Duracao FROM Filmes where Nome = 'de volta para o futuro'
+
+SELECT Nome,Ano,Duracao FROM Filmes where Ano > 2000
+
+SELECT Nome,Ano,Duracao FROM Filmes where Duracao > 100 and Duracao < 150
+
+SELECT Nome,Ano,Duracao FROM Filmes where Duracao > 100 and Duracao < 150
+
